7f4245bfe997b782adba5f856f687b097573eaca
tukurou06 <tukurou06@gmail.com>
2017-01-03 08:37:11 +0900

    add pick.js

--- docs/js/myscript.js
+++ docs/js/myscript.js
@@ -2,7 +2,12 @@ $(function(){
     //alert("ready");
 
     $("#box1").on("click",function(){
-        alert("box1 clicked!");
+        //alert("box1 clicked!");
+        Puck.eval("LED1.set()");
+    });
+
+    $("#box2").on("click",function(){
+        Puck.eval("LED1.reset()");
     });
 
 });
